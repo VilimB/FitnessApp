2ff21d72506cc8f84bdee1c40f4c773778679f6a
VilimB <benkovicvilim@gmail.com>
2024-01-26 18:30:42 +0100

    1

---
+++ app/src/main/java/ba/sum/fpmoz/fitnessapp/MainActivity.java
@@ -0,0 +1,20 @@
+package ba.sum.fpmoz.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trening);
+
+    }
+}
---
+++ app/src/main/java/ba/sum/fpmoz/fitnessapp/activity_trening.java
@@ -0,0 +1,14 @@
+package ba.sum.fpmoz.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_trening extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trening);
+    }
+}
